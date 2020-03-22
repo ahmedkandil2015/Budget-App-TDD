934bda0e417d7985d2e6aa6b5ceebf47a67e2822
Ahmed Kandil <ahmedkandil2015@hotmail.com>
2020-03-22 09:53:47 +0200

    create transaction feature test file

---
+++ tests/Feature/ViewTransactionTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class ViewTransactionTest extends TestCase
+{
+    use DatabaseMigrations;
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testICanDisplayAllTransactions()
+    {
+        $transaction = factory('App\Transaction')->create();
+        $this->get('/transactions')
+            ->assertSee($transaction->description);
+
+    }
+}
