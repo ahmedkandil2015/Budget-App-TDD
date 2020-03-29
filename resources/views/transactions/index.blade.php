0d2bd5732dd61ab574069e4507076a0410a3fe1d
Ahmed Kandil <ahmedkandil2015@hotmail.com>
2020-03-29 10:08:51 +0200

    create transaction controller and view and pass first tdd test

---
+++ resources/views/transactions/index.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app')
+<div class="container">
+    <div class="table-responsive">
+        <table>
+            <thead>
+            <tr>
+                <td>Date</td>
+                <td>Description</td>
+                <td>Amount</td>
+            </tr>
+            </thead>
+            <tbody>
+            @if($transactions->count()>0)
+                @foreach($transactions as $transaction)
+                    <tr>
+                        <td>{{$transaction->created_at->format('m/d/y')}}</td>
+                        <td>{{$transaction->description}}</td>
+                        <td>{{$transaction->amount}}</td>
+                    </tr>
+                @endforeach
+            @endif
+            </tbody>
+        </table>
+    </div>
+</div>
